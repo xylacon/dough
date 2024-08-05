73319362013b26656a43146ec4e204ae83ccbf44
Miles Bi <miles.bi.jiayu@gmail.com>
2024-08-05 03:59:43 -0500

    newstuff

--- README.md
+++ README.md
@@ -2,7 +2,25 @@
 
 ## About the project
 
-### Creating a receipt machine learning model
+### Creating a receipt information extraction model
+
+The steps for building the machine learning model are performed within the directory `receipt_ml`.
+
+#### Preparing the data
 
 1. First, images of receipts were acquired from online datasets.
    The sources are linked in a text file titled `img_sources.txt`.
+   The following are some assumptions:
+   - All images are JPEG files.
+   - They have unique names.
+   - They are located in a folder called `receipt_ml/image_dataset`.
+
+Next, a ground truth dataset corresponding to each image was created.
+To create the ground truth, the API for [GPT-4o](https://openai.com/api/) by OpenAI was used.
+
+2. Prepare the data for use with the Batch API.
+   To begin, run `gpt_json.py` inside the `data_preparation` directory.
+
+   The script uses the prompt given in `gpt_prompt.txt` and the image dataset to wrap the data in JSONL files as required by Batch API.
+   The task files generated are located in `data_preparation/batches`.
+   Note that due to restrictions on API usage, multiple small task files are created, and hence, multiple Batch API requests are needed.
---
+++ receipt_ml/data_preparation/gpt_json.py
@@ -0,0 +1,106 @@
+import json
+import base64
+from tqdm import tqdm
+from pathlib import Path
+
+
+def all_batch(img_files:list[Path], prompt:str):
+    # initialize tasks
+    tasks = []
+
+    # image operations
+    for img_path in tqdm(img_files):
+
+        with open(img_path, mode="rb") as img_file:
+            # get image id
+            img_id = img_path.stem
+
+            # convert image to base64 bytes
+            img = base64.b64encode(img_file.read()).decode("utf-8")
+
+        # create task line
+        task = {
+            "custom_id": f"{img_id}",
+            "method": "POST",
+            "url": "/v1/chat/completions",
+            "body": {
+                "model": "gpt-4o",
+                "messages": [
+                    {"role": "system", "content": "You are a helpful document parsing assistant."},
+                    {"role": "user", 
+                        "content": [
+                            {
+                                "type": "image_url",
+                                "image_url": {
+                                    "url": f"data:image/jpeg;base64,{img}",
+                                    "detail": "high"
+                                }
+                            },
+                            {
+                                "type": "text",
+                                "text": prompt
+                            }
+                        ]
+                    },
+                ],
+                "max_tokens": 1000
+            }
+        }
+
+        tasks.append(task)
+
+    # batch task file path
+    batch_path = Path("batch_tasks.jsonl")
+
+    # write to jsonl file
+    with open(batch_path, mode="w") as batch_file:
+        for task in tasks:
+            batch_file.write(json.dumps(task) + "\n")
+
+
+def divide_list(src:list, n:int) -> list[list]:
+    # initialize
+    result = []
+
+    # compute chunk size
+    size = len(src)
+    chunk_size = (size // n) + 1
+
+    for i in range(0, size, chunk_size):
+        result.append(src[i:i+chunk_size])
+
+    return result
+
+
+if __name__=="__main__":
+
+    # get the prompt
+    with open("gpt_prompt.txt", mode="r") as prompt_file:
+        gpt_instructions = "".join([x for x in prompt_file])
+
+    # get a list of all image paths
+    files = [x for x in Path("../image_dataset").rglob("*.jpg")]
+
+    # create a single batch file containing all batch tasks
+    all_batch(files, gpt_instructions)
+
+    # open the jsonl
+    with open("batch_tasks.jsonl", mode="r") as batch_file:
+        data = [json.loads(line) for line in batch_file]
+
+    # delete the single batch file
+    Path("batch_tasks.jsonl").unlink()
+
+    # divide up the jsonl data into appropriate size chunks (for this dataset, n=200)
+    n = 400
+    batches = divide_list(data, n)
+
+    # smaller batches directory
+    batch_dir = Path("batches/")
+    batch_dir.mkdir(exist_ok=True)
+
+    # write to smaller jsonl files in batches directory
+    for i in tqdm(range(n)):
+        with open(Path(batch_dir / f"batch_{i}.jsonl"), mode="w") as batch_file:
+            for task in batches[i]:
+                batch_file.write(json.dumps(task) + "\n")
